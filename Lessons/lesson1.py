4054206caf5d3fd40b03db4f2ade308d877fe6b7
Ghost <ghost_sec23@icloud.com>
2025-02-21 10:34:31 -0700

    Update lesson1.py

--- Lessons/lesson1.py
+++ Lessons/lesson1.py
@@ -28,3 +28,25 @@ print("Hello, " + name + "!")  # This will print: Hello, Alice!
 # print("Hello, " + name + "!")
 # age = 25
 # print("I am", age, "years old.")
+
+# What is a Comment?
+# Comments are lines in the code that are not executed. They are used to explain and document the code.
+# In Python, comments start with the '#' symbol.
+
+# More Examples of Variables:
+# Storing a number
+age = 30  # Here, we are storing the number 30 in a container called 'age'.
+print("I am", age, "years old.")  # This will print: I am 30 years old.
+
+# Storing a floating-point number
+height = 5.9  # Here, we are storing the number 5.9 in a container called 'height'.
+print("My height is", height, "feet.")  # This will print: My height is 5.9 feet.
+
+# Storing a boolean value
+is_student = True  # Here, we are storing the boolean value True in a container called 'is_student'.
+print("Am I a student?", is_student)  # This will print: Am I a student? True
+
+# Experiment with Variables:
+# 1. Create variables to store your favorite color, hobby, and whether you like Python.
+# 2. Print these variables to see the output.
+# 3. Change the values of these variables and print the new values.
